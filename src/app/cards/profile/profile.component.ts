1536fe4b392069f1ae745b137d1c50d536b6968f
stepanusjanu19 <kitoyu22@gmail.com>
2025-11-19 16:45:53 +0700

    feat: refactor code card profile component text
    - changes => tailwind style for desktop and mobile responsive designa

--- src/app/cards/profile/profile.component.ts
+++ src/app/cards/profile/profile.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import confetti from 'canvas-confetti';
-import { environment } from '../../../environments/environment';
+import { Component, OnInit, HostListener } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import confetti from "canvas-confetti";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'app-profile',
+  selector: "app-profile",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
   showPopup = false;
@@ -18,8 +18,8 @@ export class ProfileComponent implements OnInit {
   baseurl: string = environment.urlApi;
   fullname: string = "Stepanus Janu Adi Nugroho";
   fulltext: string = `I love building the logic behind the scenes, crafting APIs, and exploring new technologies that make web
-                      applications powerful and scalable.Curious, always learning, and driven to grow — I’m on a journey to master
-                      the backend and bring ideas to lifethrough clean, efficient code. 🚀`
+                      applications powerful and scalable. Curious, always learning, and driven to grow — I’m on a journey to master
+                      the backend and bring ideas to lifethrough clean, efficient code. 🚀`;
 
   celebrate() {
     const duration = 3000; // in milliseconds
@@ -32,7 +32,6 @@ export class ProfileComponent implements OnInit {
 
     // Clear confetti after a certain duration
     setTimeout(() => confetti.reset(), duration);
-    
   }
 
   ngOnInit(): void {
@@ -44,35 +43,33 @@ export class ProfileComponent implements OnInit {
       // Trigger the animation after showing the popup
       setTimeout(() => {
         this.animatePopup = true;
-      }, 50);  // Small delay to allow DOM to render
+      }, 50); // Small delay to allow DOM to render
     }, 2500); // 3 seconds delay for popup
-  
   }
 
-  @HostListener('window:resize')
+  @HostListener("window:resize")
   onResize() {
     this.updateGridCols(window.innerWidth);
   }
 
   updateGridCols(windowWidth: number) {
-    this.gridCols = windowWidth >= 1024 ? 'col-span-2' :
-      windowWidth >= 640 ? 'col-span-2' :
-        'col-span-1';
+    this.gridCols =
+      windowWidth >= 1024
+        ? "col-span-2"
+        : windowWidth >= 640
+          ? "col-span-2"
+          : "col-span-1";
   }
-  
+
   getGreeting(): string {
     const currentHour = new Date().getHours();
 
     if (currentHour < 12) {
-      return 'Good Morning';
+      return "Good Morning";
     } else if (currentHour < 18) {
-      return 'Good Afternoon';
+      return "Good Afternoon";
     } else {
-      return 'Good Evening';
+      return "Good Evening";
     }
   }
-
-
-  
 }
-
